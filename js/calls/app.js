3a750a7bb0ee5bef060a4621a86b7c224569f880
BakedMilk <agwzd9@gmail.com>
2017-07-13 14:04:47 +0700

    add wave to slider

--- js/calls/app.js
+++ js/calls/app.js
@@ -2,7 +2,3 @@
 $( ".express-delivery-button" ).click(function( event ) {
   event.preventDefault();
 });
-$( "#actions" ).click(function( event ) {
-  $( this ).hide();
-  console.log('it works!');
-});
--- js/calls/main-slider.js
+++ js/calls/main-slider.js
@@ -1,5 +1,12 @@
+$('.slider-slick').on('init', function(event, slick){
+  $('.slider-slick').find('.slick-track').append('<div class="slider-wave"></div>');
+});
 $('.slider-slick').slick({
 	dots: true,
 	arrows: false,
-	fade: true,
+	//infinite: false
 });
+/*$('.slider-slick').on('init', function(event, slick){
+    // let's do this after we init the banner slider
+    console.log('slider was initialized');
+});*/
